9abbe72572713166d3cbe627e06683c0ce662c9c
Brad Keller <80160556+Bradkeller2117@users.noreply.github.com>
2021-08-24 18:32:44 -0400

    adding if else statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,36 @@
-var playerName = window.prompt("What is your fighters name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("our robots name is " + playerName);
+var playerName = window.prompt("what is your fighters name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight () {
-    window.alert("The fight has begun")
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Rumble!");
+   
+enemyHealth = enemyHealth - playerAttack;
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+if (enemyHealth <= 0){
+    window.alert(enemyName + " has died! ");
 }
-//fight();
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+}
+
+  
+playerHealth = playerHealth - enemyAttack;
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died.");
+}
+else{
+    window.alert(playerName + " still has " + playerHealth + " health remaining ");
+}  
+
+};
+fight();
